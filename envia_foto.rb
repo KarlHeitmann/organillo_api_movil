a211fe1a5f6ee504c65d48017a31c7f38a1f7ddb
Karl Heitmann <khf1988@gmail.com>
2016-06-07 13:15:21 -0400

    envia foto cambiado para nueva app

--- envia_foto.rb
+++ envia_foto.rb
@@ -1,5 +1,8 @@
 require 'net/http'
 
+def masajear
+  puts "poto"
+end
 Shoes.app width: 400 do
   background rgb(240, 250, 208)
 
@@ -13,32 +16,67 @@ Shoes.app width: 400 do
       @dispatch_id = edit_line
       @dispatch_id.text = "1"
     end
+    flow do 
+      para "Comentario"
+      @comentario = edit_line
+      @comentario.text = "Lorem ipsum"
+    end
+    stack do
+      flow do
+        radio do
+          @lat = "1"
+          @lng = "2"
+          @instante = "3"
+          puts @lat, @lng, @instante
+        end
+        para "Posicion 1"
+      end
+      flow do
+        radio do 
+          @lat="4"
+          @lng="5"
+          @instante = "6"
+          puts @lat, @lng, @instante
+          masajear
+        end
+        para "Posicion 2"
+      end
+    end
   end
   stack width: 200 do
+    button "test" do
+      puts @lat
+      masajear
+
+    end
     button "Enviar apple" do
       params = {
-        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 manzana.jpg`}"
+        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 manzana.jpg`}",
+        'comentario' => @comentario.text
       }
       res = Net::HTTP.post_form(@uri, params)
       alert res.body
     end
     button "Enviar banana" do
       params = {
-        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 banana.png`}"
+        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 banana.png`}",
+        'comentario' => @comentario.text
       }
       res = Net::HTTP.post_form(@uri, params)
       alert res.body
     end
     button "Enviar orange" do
       params = {
-        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 orange.png`}"
+        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 orange.png`}",
+        'comentario' => @comentario.text
       }
       res = Net::HTTP.post_form(@uri, params)
       alert res.body
     end
     button "Enviar lena" do
       params = {
-        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 lena.bmp`}"
+        'dispatch_id' => @dispatch_id.text, 'data_raw' => "data:image/jpeg;base64,#{`base64 --wrap=0 lena.bmp`}",
+        'comentario' => @comentario.text
       }
       res = Net::HTTP.post_form(@uri, params)
       alert res.body
