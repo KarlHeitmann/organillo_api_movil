d0440d73ebbbe5f5abe74d164fde00c2f1c27f85
Karl Heitmann <khf1988@gmail.com>
2016-06-23 12:33:37 -0400

    arreglado envia_road para enviar un tramo -casi- circular

--- envia_road.rb
+++ envia_road.rb
@@ -1,15 +1,34 @@
 require 'net/http'
 
-def masajear
+LAT = -33.081
+LNG = -71.612
+
+def masajear(i)
+
+  if i < 7
+    lat = LAT + i * 0.01
+    lng = LNG + i * 0.01
+  elsif i < 14
+    lat = LAT + (7*2-i) * 0.01
+    lng = LNG + i * 0.01
+  elsif i < 21
+    lat = LAT + (7*2-i) * 0.01
+    lng = LNG + (7*4-i) * 0.01
+  elsif i < 28
+    lat = LAT + (7*4-i) * 0.01
+    lng = LNG + (7*4-i) * 0.01
+  end
+
   params = {
-    'road[lat]' => "-33.081", 'road[lng]' => "-71.612"
+    'road[lat]' => lat, 'road[lng]' => lng
   }
+  return params
 end
 
 Shoes.app width: 400 do
   background rgb(240, 250, 208)
-  @uri = URI('http://localhost:3000')
-  #@uri = URI('http://cins.resed.cl')
+  #@uri = URI('http://localhost:3000')
+  @uri = URI('http://cins.resed.cl')
   puts @uri
 
   stack width: 200 do
@@ -20,12 +39,14 @@ Shoes.app width: 400 do
     end
   end
   stack width: 200 do
-    button "Enviar salida" do
+    button "Enviar ruta 1" do
       request = @uri + "/dispatches/#{@dispatch_id.text}/roads"
-      params = masajear
-      res = Net::HTTP.post_form(request, params)
-      puts res.body
-      alert "ok"
+      28.times do |i|
+        params = masajear(i)
+        res = Net::HTTP.post_form(request, params)
+        puts res.body
+        alert "ok"
+      end
     end
   end
 end
--- envia_trace.rb
+++ envia_trace.rb
@@ -10,8 +10,8 @@ require 'net/http'
 
 Shoes.app width: 400 do
   background rgb(240, 250, 208)
-  @uri = URI('http://localhost:3000/shippings_driver_trace.json')
-  #@uri = URI('http://cins.resed.cl/shippings_driver_trace.json')
+  #@uri = URI('http://localhost:3000/shippings_driver_trace.json')
+  @uri = URI('http://cins.resed.cl/shippings_driver_trace.json')
   puts @uri
 
   stack width: 200 do
