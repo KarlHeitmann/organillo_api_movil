53e5d0732cadbdd3c4647a36a16752516cd9a5cd
Karl Heitmann <khf1988@gmail.com>
2016-09-12 13:05:46 -0300

    agregado archivo prueba.rb, para probar el envio de archivos a traves de simple lenguaje ruby

---
+++ prueba.rb
@@ -0,0 +1,10 @@
+require 'net/http/post/multipart'
+
+url = URI.parse('http://www.example.com/upload')
+File.open("./image.jpg") do |jpg|
+  req = Net::HTTP::Post::Multipart.new url.path,
+    "file" => UploadIO.new(jpg, "image/jpeg", "image.jpg")
+  res = Net::HTTP.start(url.host, url.port) do |http|
+    http.request(req)
+  end
+end
