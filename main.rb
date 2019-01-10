927fe2d7f2ec41a1f0a37272247466f4cf65cf1b
Karl Heitmann <khf1988@gmail.com>
2019-01-09 21:22:46 -0300

    en main agregado boton para obtener autenticacion

--- main.rb
+++ main.rb
@@ -19,12 +19,47 @@ Shoes.app width: 400 do
   @config_data = @config_data["Parametros web"]
 
   stack width: 200 do
+    flow do
+      para "Email" #Corresponde a nombre del contenedor
+      @email = edit_line
+      @email.text = "khf_88@yahoo.es"
+    end
+    flow do
+      para "Password"
+      @password = edit_line
+      @password.text = "asdasd"
+    end
+
+
+    button "Obtener autenticacion" do
+      uri = URI("#{@config_data["dominio"]}/authenticate.json")
+      puts uri
+      params = {
+        'sign_in[email]' => @email.text,
+        'sign_in[password]' => @password.text,
+        #'sign_in[role_id]' => ROL_APLICACION[_tipo_usuario_movil]
+      }
+      puts params
+      puts uri
+      res = Net::HTTP.post_form(uri, params)
+      puts res.body
+      #alert res.body
+      @token = JSON.parse(res.body)["auth_token"]
+      @current_usuario = JSON.parse(res.body)["user"]
+      puts @token
+    end
+
+
     button "Fotos" do
       envia_foto
     end
     button "Traces" do
       envia_trace
     end
+    button "Traces" do
+      envia_trace
+    end
+
   end
 end
 
